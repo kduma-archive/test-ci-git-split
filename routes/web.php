01093d5ef7cc5b092080af94e594d43bff94f04e
Krystian Duma <git@krystian.duma.sh>
2023-12-20 04:46:25 +0100

    Test

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'welcome');
 
 Route::get('/lib', function (\TestCi\Library\Lib $lib) {
-    return $lib->hello();
+    return $lib->hello('Laravel');
 });
 
 Route::view('dashboard', 'dashboard')
