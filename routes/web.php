0b3423cf861ec6c5087746f0bd58bbd6829efc5b
Krystian Duma <git@krystian.duma.sh>
2023-12-20 05:04:56 +0100

    fix: change name

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'welcome');
 
 Route::get('/lib', function (\TestCi\Library\Lib $lib) {
-    return $lib->hello('Joe');
+    return $lib->hello('John');
 });
 
 Route::view('dashboard', 'dashboard')
